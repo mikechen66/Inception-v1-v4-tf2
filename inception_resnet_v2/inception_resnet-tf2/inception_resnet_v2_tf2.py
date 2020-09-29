be329f819b8bfe4fa0f13c7afdd9c1f22e221985
mikechen66 <875661606@qq.com>
2020-09-29 15:14:39 +0800

    Update the script

--- inception_resnet_v2/inception_resnet-tf2/inception_resnet_v2_tf2.py
+++ inception_resnet_v2/inception_resnet-tf2/inception_resnet_v2_tf2.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
 
 # inception_resnet_v2_tf2.py
 
 """
-The inception_resnet a, b and c blocks are 35 x 35, 17 x 17 and 8 x 8 in the gride size. Please note 
-that the filters in the joint convoluation for inception_resnet a, b and c blocks are respectively 384, 
-1154 and 2048.  
+The Inception-Resnet A, B and C blocks are 35 x 35, 17 x 17 and 8 x 8 respectively in the gride size. 
+Please note the filters in the joint convoluation for A B and C blocks are respectively 384, 1154 and 
+2048. It is a variant without an auxiliary classifier. Please run the 
 
 $ python inception_resnet_v2_tf2.py
 
@@ -13,14 +15,10 @@ we do not set the argument of the weights that need to be downloaded from design
 
 Make the the necessary changes to adapt to the environment of TensorFlow 2.3, Keras 2.4.3, CUDA Toolkit 
 11.0, cuDNN 8.0.1 and CUDA 450.57. In addition, write the new lines of code to replace the deprecated 
-code. 
+code.  
 
-The script has many changes on the foundation of is Inception v3 by Francios Chollet, Inception v4 by 
-Kent Sommers and many other published results. I would like to thank all of them for the contributions. 
-
-Note that the input image format for this model is different than for the VGG16 and ResNet models 
-(299x299 instead of 224x224), and that the input preprocessing function is also different (same as 
-Xception).
+Please note that the input image format for this model is different than for the VGG16 and ResNet models 
+(299x299 instead of 224x224), and that the input preprocessing function is also different. 
 
 Environment: 
 Ubuntu 18.04 
@@ -28,10 +26,12 @@ TensorFlow 2.3
 Keras 2.4.3
 CUDA Toolkit 11.0, 
 cuDNN 8.0.1
-CUDA 450.57. 
+CUDA 450.57.
 
+Inception-v4, Inception-ResNet and the Impact of Residual Connections on Learning
 https://arxiv.org/pdf/1602.07261.pdf
 """
+
 
 import tensorflow as tf 
 from tensorflow.keras.layers import Input, Conv2D, Dropout, Dense, Lambda, Flatten, Activation, \
@@ -47,28 +47,28 @@ for gpu in gpus:
 
 
 def inception_stem(input):
-    # Define the stem network--keep no chnaged --INception v4
-    a = Conv2D(filters=32, kernel_size=(3,3), activation='relu', strides=(2,2))(input)
-    a = Conv2D(filters=32, kernel_size=(3,3), activation='relu')(a)
-    a = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(a)
+    # Define the stem network as similar as Inception v4. 
+    a = Conv2D(filters=32, kernel_size=(3,3), strides=(2,2), activation='relu', padding='valid')(input)
+    a = Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), activation='relu', padding='valid')(a)
+    a = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same')(a)
 
-    b1 = MaxPooling2D((3,3), strides=(2,2))(a)
-    b2 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', strides=(2,2))(a)
+    b1 = Conv2D(filters=96, kernel_size=(3,3), strides=(2,2), activation='relu', padding='valid')(a)
+    b2 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(a)
 
     b = concatenate([b1, b2], axis=3)
 
-    c1 = Conv2D(filters=64, kernel_size=(1,1), activation='relu', padding='same')(b)
-    c1 = Conv2D(filters=96, kernel_size=(3,3), activation='relu')(c1)
+    c1 = Conv2D(filters=64, kernel_size=(1,1), strides=(1,1), activation='relu', padding='same')(b)
+    c1 = Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), activation='relu', padding='valid')(c1)
 
-    c2 = Conv2D(filters=64, kernel_size=(1,1), activation='relu', padding='same')(b)
-    c2 = Conv2D(filters=64, kernel_size=(7,1), activation='relu', padding='same')(c2)
-    c2 = Conv2D(filters=64, kernel_size=(1,7), activation='relu', padding='same')(c2)
-    c2 = Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='valid')(c2)
+    c2 = Conv2D(filters=64, kernel_size=(1,1), strides=(1,1), activation='relu', padding='same')(b)
+    c2 = Conv2D(filters=64, kernel_size=(7,1), strides=(1,1), activation='relu', padding='same')(c2)
+    c2 = Conv2D(filters=64, kernel_size=(1,7), strides=(1,1), activation='relu', padding='same')(c2)
+    c2 = Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), activation='relu', padding='valid')(c2)
 
     c = concatenate([c1, c2], axis=3)
 
-    d1 = MaxPooling2D((3,3), strides=(2,2))(c)
-    d2 = Conv2D(filters=192, kernel_size=(3,3), activation='relu', strides=(2,2))(c)
+    d1 = Conv2D(filters=192, kernel_size=(3,3), activation='relu', strides=(2,2), padding='valid')(c)
+    d2 = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid')(c)
 
     d = concatenate([d1, d2], axis=3)
     d = BatchNormalization(axis=3)(d)
@@ -92,7 +92,7 @@ def inception_a(input, scale):
 
     e = Conv2D(filters=384, kernel_size=(1,1), activation='linear', padding='same')(e)
 
-    if scale: e = Lambda(lambda x: x*0.15)(e)
+    if scale: e = Lambda(lambda z: z*0.15)(e)
 
     e = concatenate([input, e], axis=3)
     e = BatchNormalization(axis=3)(e)
@@ -101,19 +101,17 @@ def inception_a(input, scale):
     return e
 
 
-def reduction_a(input):
-    # Define Reduction-A: keep no changed--Inception v4
-    f1 = MaxPooling2D((3,3), strides=(2,2))(input)
+def reduction_a(input): 
+    # Define Reduction-A: 35 x 35 --> 17 x 17 
+    f1 = Conv2D(filters=384, kernel_size=(3,3), strides=(2,2), activation='relu', padding='valid')(input)
 
-    f2 = Conv2D(filters=384, kernel_size=(3,3), activation='relu', strides=(2,2))(input)
+    f2 = Conv2D(filters=256, kernel_size=(1,1), strides=(1,1), activation='relu', padding='same')(input)
+    f2 = Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same')(f2)
+    f2 = Conv2D(filters=384, kernel_size=(3,3), strides=(2,2), activation='relu', padding='valid')(f2)
 
-    f3 = Conv2D(filters=192, kernel_size=(1,1), activation='relu', padding='same')(input)
-    f3 = Conv2D(filters=224, kernel_size=(3,3), activation='relu', padding='same')(f3)
-    f3 = Conv2D(filters=256, kernel_size=(3,3), activation='relu', strides=(2,2))(f3)
+    f3 = MaxPooling2D((3,3), strides=(2,2), padding='valid')(input)
 
     f = concatenate([f1, f2, f3], axis=3)
-    f = BatchNormalization(axis=3)(f)
-    f = Activation('relu')(f)
 
     return f
 
@@ -130,7 +128,7 @@ def inception_b(input, scale):
 
     g = Conv2D(filters=1154, kernel_size=(1,1), activation='linear', padding='same')(g)
 
-    if scale: g = Lambda(lambda x: x*0.10)(g)
+    if scale: g = Lambda(lambda z: z*0.10)(g)
 
     g = concatenate([input, g], axis=3)
     g = BatchNormalization(axis=3)(g)
@@ -140,18 +138,18 @@ def inception_b(input, scale):
 
 
 def reduction_b(input):
-    # Adopt the inception_a from the Inception v4
-    h1 = MaxPooling2D((3,3), strides=(2,2),padding='valid')(input)
+    # Define Reduction-A: 17 x 17 --> 8 x 8  
+    h1 = Conv2D(filters=256, kernel_size=(1,1), activation='relu', padding='same')(input)
+    h1 = Conv2D(filters=384, kernel_size=(3,3), activation='relu', strides=(2,2), padding='valid')(h1)
 
     h2 = Conv2D(filters=256, kernel_size=(1,1), activation='relu', padding='same')(input)
-    h2 = Conv2D(filters=384, kernel_size=(3,3), activation='relu', strides=(2,2))(h2)
+    h2 = Conv2D(filters=288, kernel_size=(3,3), activation='relu', strides=(2,2), padding='valid')(h2)
 
     h3 = Conv2D(filters=256, kernel_size=(1,1), activation='relu', padding='same')(input)
-    h3 = Conv2D(filters=288, kernel_size=(3,3), activation='relu', strides=(2,2))(h3)
+    h3 = Conv2D(filters=288, kernel_size=(3,3), activation='relu', padding='same')(h3)
+    h3 = Conv2D(filters=320, kernel_size=(3,3), activation='relu', strides=(2,2), padding='valid')(h3)
 
-    h4 = Conv2D(filters=256, kernel_size=(1,1), activation='relu', padding='same')(input)
-    h4 = Conv2D(filters=288, kernel_size=(3,3), activation='relu', padding='same')(h4)
-    h4 = Conv2D(filters=320, kernel_size=(3,3), activation='relu', strides=(2,2))(h4)
+    h4 = MaxPooling2D((3,3), strides=(2,2),padding='valid')(input)
 
     h = concatenate([h1, h2, h3, h4], axis=3)
     h = BatchNormalization(axis=3)(h)
@@ -172,7 +170,7 @@ def inception_c(input, scale):
 
     i = Conv2D(filters=2048, kernel_size=(1,1), activation='linear',padding='same')(i)
 
-    if scale: i = Lambda(lambda x: x*0.20)(i)
+    if scale: i = Lambda(lambda z: z*0.20)(i)
 
     i = concatenate([input, i], axis=3)
     i = BatchNormalization(axis=3)(i)
@@ -181,24 +179,17 @@ def inception_c(input, scale):
     return i
 
 
-def aux_output(x):
-    # Define an auxiliary classifier 
-    y = AveragePooling2D(pool_size=(5,5), strides=(3,3))(x)
-    y = Conv2D(filters=128, kernel_size=(1,1),padding='same', activation='relu')(y)
-    y = GlobalAveragePooling2D(name='aux_gav_pool')(y)
-    y = Dense(units=768, activation='relu')(y)
-    y = Dropout(0.2)(y)
-    y = Dense(num_classes, activation='softmax', name="aux_pred")(y)
-
-    return y 
-
-
-def inception_resnet_v2(input_shape, num_classes, include_top):
-    # Build the inception resnet v2 network
+def inception_resnet_v2(input_shape, num_classes, include_top, weights):
+    # Build the abstract Inception v4 network
     """
-    :param nb_classes: number of classes.txt
-    :param scale: flag to add scaling of activations
-    :return: 1 input (299x299x3) input shape and 2 outputs (output,aux)
+    Args:
+    input_shape: three dimensions in the TensorFlow Data Format
+    num_classes: number of classes
+    weights: pre-defined Inception v4 weights 
+    include_top: a boolean, for full traning or finetune 
+
+    Return: 
+    logits: the logit outputs of the model.
     """
     # Initizate a 3D shape(weight,height,channels) into a 4D tensor(batch, weight, 
     # height, channels). If no batch size, it is defaulted as None.
@@ -210,17 +201,14 @@ def inception_resnet_v2(input_shape, num_classes, include_top):
     for i in range(10):
         x = inception_a(x, scale=True)
 
-    # Reduction A
+    # Reduction A - 35 x 35 --> 17 x 17 
     x = reduction_a(x)
 
     # 20 x Inception Resnet B
     for i in range(20):
         x = inception_b(x, scale=True)
 
-    # Please note it is the auxiliary classifier
-    y = aux_output(x)
-
-    # Reduction B
+    # Reduction B - 17 x 17 --> 8 x 8
     x = reduction_b(x)
 
     # 10 x Inception Resnet C
@@ -235,7 +223,7 @@ def inception_resnet_v2(input_shape, num_classes, include_top):
         x = Dense(units=num_classes, activation='softmax', name='main_pred')(x)
 
     # Build the model 
-    model = Model(inputs, [x, y], name='Inception-Resnet-v2')
+    model = Model(inputs, x, name='Inception-Resnet-v2')
 
     return model
 
@@ -243,10 +231,9 @@ def inception_resnet_v2(input_shape, num_classes, include_top):
 if __name__ == '__main__':
 
     input_shape = (299,299,3)
-    num_classes = 1000 
+    num_classes = 1000
     include_top = True 
-    weights = None
+    weights = None 
 
-    model = inception_resnet_v2(input_shape, num_classes, include_top)
-
+    model = inception_resnet_v2(input_shape, num_classes, include_top, weights)
     model.summary()
